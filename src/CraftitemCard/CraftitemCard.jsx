becece09ca970fe46e22ec9e10eef1c7c96d70ae
Joy <github.joysaha@gmail.com>
2024-04-26 20:31:07 +0600

    fetch data from database and show data on home page

--- src/CraftitemCard/CraftitemCard.jsx
+++ src/CraftitemCard/CraftitemCard.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const CraftitemCard = () => {
-    return (
-        <div>
-            <h2>hello</h2>
+const CraftitemCard = ({ item }) => {
+  return (
+    <div>
+      <div className="card w-full bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CraftitemCard;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import Slider from '../../Components/Slider/Slider'
-import CraftItemSec from '../../Components/CraftItemSec/CraftItemSec';
+import React from "react";
+import Slider from "../../Components/Slider/Slider";
+import CraftItemSec from "../../Components/CraftItemSec/CraftItemSec";
+import { useLoaderData } from "react-router-dom";
+import CraftitemCard from "../../CraftitemCard/CraftitemCard";
 
 const Home = () => {
-    return (
-        <div>
-            <Slider></Slider>
-            <CraftItemSec></CraftItemSec>
+  const itemdata = useLoaderData();
+  
+
+  return (
+    <div>
+      <Slider></Slider>
+      {/* <CraftItemSec></CraftItemSec> */}
+      <div className="max-w-6xl mx-auto mt-6">
+        <div className="text-center space-y-4">
+          <h2 className="text-xl md:text-3xl font-bold">Popular Craft Items</h2>
+          <p className="w-full mx-auto px-3 md:w-[700px]">
+            This form is use only added your art and craft item where you can
+            show your creativity item and you make you can add our website and
+            show it's a public.
+          </p>
         </div>
-    );
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+          {itemdata.map((item) => (
+            <CraftitemCard key={item._id} item={item}></CraftitemCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
