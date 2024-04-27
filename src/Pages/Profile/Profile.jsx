8c809a77c6118f323c008c131700537a710b129e
Joy <github.joysaha@gmail.com>
2024-04-27 19:16:31 +0600

    update profile and profile sectin created

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from "react";
+import useAuth from "../../Hooks/useAuth";
+
 
 const Profile = () => {
-    return (
-        <div>
-            <h2>Profile sec</h2>
-        </div>
-    );
+  const { user } = useAuth();
+  return (
+    <div className="max-w-6xl mx-auto ">
+      
+      <div className="flex flex-col items-center my-16">
+        <img
+          className="rounded-full"
+          src={user?.photoURL || "https://i.ibb.co/6JyZF0K/user.png"}
+          alt="pic"
+        />
+        <h2 className="mt-10 font-medium text-4xl">{user?.displayName}</h2>
+        <p className="text-lg mt-3">{user?.email}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
--- src/Pages/UpdateProfile/UpdateProfile.jsx
+++ src/Pages/UpdateProfile/UpdateProfile.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React  from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import useAuth from "../../Hooks/useAuth";
 
 const UpdateProfile = () => {
-    return (
-        <div>
-            <h2>Update profile</h2>
+  const { user, updateUserProfile } = useAuth();
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const {name, image} = data;
+    updateUserProfile(name, image)
+        .then(() => {
+            toast.success("Update Changes")
+            navigate('/profile')
+        })
+  };
+
+  return (
+    <div className="max-w-6xl mx-auto">
+      
+      <div className="">
+        <div className="flex flex-col items-center ">
+          <img
+            className="rounded-full"
+            src={user?.photoURL || "https://i.ibb.co/6JyZF0K/user.png"}
+            alt="pic"
+          />
+          <h2 className="my-5 font-medium text-4xl">{user?.displayName}</h2>
+          <p className="text-lg mb-2">{user?.email}</p>
         </div>
-    );
+        <div className="flex flex-col items-center">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body lg:w-[30%] border">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="name"
+                defaultValue={user?.displayName}
+                className="input input-bordered rounded-none"
+                {...register("name", { required: true })}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Photo URL"
+                className="input input-bordered rounded-none"
+                {...register("image")}
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn bg-[#0077be] text-white hover:text-black rounded-none ">
+                Save Changes
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UpdateProfile;
