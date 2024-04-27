f8d758086c8fa18eb1789ade428d752c195772a5
Joy <github.joysaha@gmail.com>
2024-04-27 09:54:42 +0600

    art and craft pages created

---
+++ src/Pages/ArtandCraftCategory/ArtandCraftCategory.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ArtandCraftCategory = () => {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <div className="text-center space-y-4">
+        <h2 className="text-xl md:text-3xl font-bold">
+          Art & Craft Categories
+        </h2>
+        <p className="w-full mx-auto px-3 md:w-[700px]">
+          This section all card is art and craft category where you can select and chose your category wise items. this section help to a find this art craft items is shortly.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ArtandCraftCategory;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import Slider from "../../Components/Slider/Slider";
 import { Link, useLoaderData } from "react-router-dom";
 import CraftitemCard from "../../CraftitemCard/CraftitemCard";
+import ArtandCraftCategory from "../ArtandCraftCategory/ArtandCraftCategory";
 
 const Home = () => {
   const itemdata = useLoaderData();
-  
 
   return (
     <div>
       <Slider></Slider>
-      {/* <CraftItemSec></CraftItemSec> */}
       <div className="max-w-6xl mx-auto mt-6">
         <div className="text-center space-y-4">
-          <h2 className="text-xl md:text-3xl font-bold">Popular Craft Items</h2>
+          <h2 className="text-xl md:text-3xl font-bold">
+            Popular Art & Craft Items
+          </h2>
           <p className="w-full mx-auto px-3 md:w-[700px]">
-            This form is use only added your art and craft item where you can
-            show your creativity item and you make you can add our website and
-            show it's a public.
+            This section where you can find get a popular art and craft and if
+            you find your requirement art so you can click our view more button
+            and you can get our all craft and art items.
           </p>
         </div>
         <div className="grid grid-cols-1 px-3 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
@@ -26,9 +27,12 @@ const Home = () => {
           ))}
         </div>
         <div className="text-center my-6">
-            <Link to={'/allartandcraft'} className="btn btn-accent">View More</Link>
+          <Link to={"/allartandcraft"} className="btn btn-accent">
+            View More
+          </Link>
         </div>
       </div>
+      <ArtandCraftCategory></ArtandCraftCategory>
     </div>
   );
 };
